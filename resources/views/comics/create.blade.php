b50a04b943c39f61d3a5f825ea8841f9e0dc18f5
LucaAgatinoPrivitera <priviteraluca01@gmail.com>
2024-07-05 15:14:28 +0200

    create aggiornato, adesso contiene tutti i campi tranne l'id (si autoincrementa)

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -11,6 +11,30 @@
                 <div>{{ $message }}</div>
             @enderror
         </div>
+
+        <div>
+            <label for="autore">autore:</label>
+            <input type="text" id="autore" name="autore" value="{{ old('autore') }}">
+            @error('autore')
+                <div>{{ $message }}</div>
+            @enderror
+        </div>
+
+        <div>
+            <label for="descrizione">descrizione:</label>
+            <input type="text" id="descrizione" name="descrizione" value="{{ old('descrizione') }}">
+            @error('descrizione')
+                <div>{{ $message }}</div>
+            @enderror
+        </div>
+
+        <div>
+            <label for="img">img:</label>
+            <input type="text" id="img" name="img" value="{{ old('img') }}">
+            @error('img')
+                <div>{{ $message }}</div>
+            @enderror
+        </div>
         <!-- Aggiungi qui altri campi del form se necessario -->
         <button type="submit">Crea</button>
     </form>
