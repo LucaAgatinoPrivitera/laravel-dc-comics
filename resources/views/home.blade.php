11a48a3c2b7ced19e4997944009b8dedb55c2677
LucaAgatinoPrivitera <priviteraluca01@gmail.com>
2024-07-05 12:17:44 +0200

    Commenti aggiunti

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,6 +6,7 @@
 		<div style="margin-bottom: 5rem">
 			<h1>Titolo: {{ $fumetto['titolo'] }}</h1>
             <a href="{{ route('show', ['id' => $fumetto->id]) }}">Link al fumetto</a>
+					 {{-- {{ route(‘comics.show’, $comic->id) }} Si può fare anche così, presumo quando si ha solo 1 controller --}}
 		</div>
 	@endforeach
 @endsection
